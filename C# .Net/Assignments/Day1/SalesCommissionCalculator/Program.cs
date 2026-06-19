e6f3bc6847f3ffe03d7a5295429d846ee6b4f305
Swayam Terode <swayamterodex@gmail.com>
2026-06-19 14:21:37 +0530

    Add SalesCommissionCalculator and leapYear projects with initial files and configurations

---
+++ C# .Net/Assignments/Day1/SalesCommissionCalculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.Write("Enter a year: ");
+        int year = Convert.ToInt32(Console.ReadLine());
+          string result = ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+                         ? year + " is a Leap Year."
+                         : year + " is not a Leap Year.";
+
+        Console.WriteLine(result);
+    }
+}
