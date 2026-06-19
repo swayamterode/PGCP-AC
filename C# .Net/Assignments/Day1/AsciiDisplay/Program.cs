2207e72bd96b74980d920e79f09ad6b457f24467
Swayam Terode <swayamterodex@gmail.com>
2026-06-19 15:06:23 +0530

    Add AsciiDisplay project with initial implementation and .gitignore

---
+++ C# .Net/Assignments/Day1/AsciiDisplay/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AsciiDisplay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+
+            for (int i = 0; i <= 255; i++)
+            {
+                // Some values are non-printable control characters, show code + char safely
+                Console.WriteLine($"{i}\t: {(char)i}");
+                count++;
+
+                if (count % 10 == 0)
+                {
+                    Console.WriteLine("\nPress any key to continue...");
+                    Console.ReadKey();
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine("\nDone displaying all ASCII characters (0-255).");
+        }
+    }
+}
