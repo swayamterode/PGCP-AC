e5a4f081c9b12d5f7398f9dbd46d164245b86553
Swayam Terode <swayamterodex@gmail.com>
2026-06-19 15:06:06 +0530

    Add absNumber project with initial implementation and .gitignore

---
+++ C# .Net/Assignments/Day1/absNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(String[] args)
+    {
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        int absoluteValue = (num < 0) ? -num : num;
+
+        Console.WriteLine("Absolute value: " + absoluteValue);
+    }
+}
