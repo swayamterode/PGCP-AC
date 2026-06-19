0b8d960151d608f44623d7994d51e4e76bd80120
Swayam Terode <swayamterodex@gmail.com>
2026-06-19 14:51:28 +0530

    Add MaxOfThreeNumbers project with initial implementation and .gitignore

---
+++ C# .Net/Assignments/Day1/MaxOfThreeNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Enter the num1: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the num2: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the num3: ");
+        int num3 = Convert.ToInt32(Console.ReadLine());
+
+        if (num1 > num2 & num1 > num3)
+        {
+            Console.WriteLine(num1 + " is greater");
+        } else if (num2 > num1 && num2>num3)
+        {
+            Console.WriteLine(num2 + " is greater");
+        }
+        else
+        {
+            Console.WriteLine(num3 + " is greater");
+        }
+    }
+}
